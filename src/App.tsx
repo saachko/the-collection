25ff6b4ed702e680d9e05aae7b627cd67ee08862
Anastasiya Sachko <baron-nastya2009@yandex.ru>
2023-02-06 18:15:16 +0300

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { Suspense, lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+const HomePage = lazy(() => import('./pages/HomePage'));
+const SignUpPage = lazy(() => import('./pages/SignUpPage'));
+const SignInPage = lazy(() => import('./pages/SignInPage'));
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage'));
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Suspense fallback="LOADING...">
+      <Routes>
+        <Route index path="/" element={<HomePage />} />
+        <Route path="signin" element={<SignInPage />} />
+        <Route path="signup" element={<SignUpPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Suspense>
+  );
 }
 
 export default App;
