d67bfbbbf0a07c4fc1c397a43a6911e2e097c8e9
Anastasiya Sachko <baron-nastya2009@yandex.ru>
2023-02-07 00:04:44 +0300

    feat: add button controls

--- src/components/Header/ButtonControls.tsx
+++ src/components/Header/ButtonControls.tsx
@@ -1,7 +1,13 @@
 import React, { memo } from 'react';
+import { Button } from 'react-bootstrap';
 
 function ButtonControls() {
-  return <div>ButtonControls</div>;
+  return (
+    <div>
+      <Button className="secondary-button me-2">Sign In</Button>
+      <Button className="primary-button">Sign Up</Button>
+    </div>
+  );
 }
 
 export default memo(ButtonControls);
