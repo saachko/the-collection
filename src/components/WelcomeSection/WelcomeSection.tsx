ab274ee0894213f2b823e83ce9a3adecb3df0f71
Anastasiya Sachko <baron-nastya2009@yandex.ru>
2023-02-08 01:21:52 +0300

    feat: add welcome section to main page

---
+++ src/components/WelcomeSection/WelcomeSection.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
+import { NavLink } from 'react-router-dom';
+
+import WelcomeSvg from './ WelcomeSvg';
+
+function WelcomeSection() {
+  const { t } = useTranslation('translation', { keyPrefix: 'home' });
+
+  return (
+    <section className="d-flex justify-content-between gap-2">
+      <div className="w-50 ms-5 align-self-center mt--10">
+        <h1>{t('mainTitle')}</h1>
+        <p>{t('mainText')}</p>
+        <NavLink to="/collections">
+          <Button className="secondary-button">{t('browseButton')}</Button>
+        </NavLink>
+      </div>
+      <WelcomeSvg />
+    </section>
+  );
+}
+
+export default WelcomeSection;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,15 @@
 import React, { memo } from 'react';
 
+import WelcomeSection from '../components/WelcomeSection/WelcomeSection';
+
 function HomePage() {
-  return <main>HomePage</main>;
+  return (
+    <main>
+      <div className="content pt-0 pb-0">
+        <WelcomeSection />
+      </div>
+    </main>
+  );
 }
 
 export default memo(HomePage);
