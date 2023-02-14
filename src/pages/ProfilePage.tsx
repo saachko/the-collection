6e972f65e6259aba4be7f9c336426e2dc482a45e
Anastasiya Sachko <baron-nastya2009@yandex.ru>
2023-02-14 15:16:22 +0300

    feat: add redirect if user is not logged in

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+import { useAppSelector } from 'hooks/useRedux';
 
 function ProfilePage() {
+  const { isLoggedIn } = useAppSelector((state) => state.user);
+
+  if (!isLoggedIn) {
+    return <Navigate to="/" />;
+  }
   return <div>ProfilePage</div>;
 }
 
