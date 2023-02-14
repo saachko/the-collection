364c9d8bcefec72c19cdac695184df1aacbd4844
Anastasiya Sachko <baron-nastya2009@yandex.ru>
2023-02-14 15:33:19 +0300

    fix: change button controls if user is logged in

--- src/components/Header/ButtonControls.tsx
+++ src/components/Header/ButtonControls.tsx
@@ -1,25 +1,41 @@
 import React, { memo } from 'react';
 import { Button } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 
 import { showModal } from 'redux/slices/modalAuthSlice';
+import { setLoggedOut } from 'redux/slices/userSlice';
 
-import { useAppDispatch } from 'hooks/useRedux';
+import { useAppDispatch, useAppSelector } from 'hooks/useRedux';
 
 function ButtonControls() {
   const { t } = useTranslation('translation', { keyPrefix: 'header' });
+  const { isLoggedIn } = useAppSelector((state) => state.user);
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
+  if (!isLoggedIn) {
+    return (
+      <div>
+        <Button
+          className="secondary-button me-2"
+          onClick={() => dispatch(showModal('signIn'))}
+        >
+          {t('signin')}
+        </Button>
+        <Button className="primary-button" onClick={() => dispatch(showModal('signUp'))}>
+          {t('signup')}
+        </Button>
+      </div>
+    );
+  }
   return (
     <div>
-      <Button
-        className="secondary-button me-2"
-        onClick={() => dispatch(showModal('signIn'))}
-      >
-        {t('signin')}
+      <Button className="secondary-button me-2" onClick={() => dispatch(setLoggedOut())}>
+        {t('logout')}
       </Button>
-      <Button className="primary-button" onClick={() => dispatch(showModal('signUp'))}>
-        {t('signup')}
+      <Button className="primary-button" onClick={() => navigate('/profile')}>
+        {t('profile')}
       </Button>
     </div>
   );
