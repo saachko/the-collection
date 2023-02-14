3fc67573f7e40940b6b538cc8fdd3861666fb05a
Anastasiya Sachko <baron-nastya2009@yandex.ru>
2023-02-14 03:49:25 +0300

    feat: add function to parse jwt token

---
+++ src/utils/functions.ts
@@ -0,0 +1,15 @@
+const parseJwt = (tokenToParse: string) => {
+  const base64Url = tokenToParse.split('.')[1];
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  const jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split('')
+      .map((c) => `%${`00${c.charCodeAt(0).toString(16)}`.slice(-2)}`)
+      .join('')
+  );
+
+  return JSON.parse(jsonPayload);
+};
+
+export default parseJwt;
