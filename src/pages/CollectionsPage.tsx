507d24d035a4ccfa93a92a45e99259068ae3024b
Anastasiya Sachko <baron-nastya2009@yandex.ru>
2023-02-19 01:05:37 +0300

    fex: fix text for logged out users

--- src/pages/CollectionsPage.tsx
+++ src/pages/CollectionsPage.tsx
@@ -3,10 +3,15 @@ import { useTranslation } from 'react-i18next';
 
 import EmptyContainer from 'components/EmptyContainer/EmptyContainer';
 
+import { useAppSelector } from 'hooks/useRedux';
+
 function CollectionsPage() {
+  const isLoggedIn = useAppSelector((state) => state.user.isLoggedIn);
   const { t } = useTranslation('translation', { keyPrefix: 'collections' });
 
-  return <EmptyContainer text={t('empty')} />;
+  return (
+    <EmptyContainer text={isLoggedIn ? `${t('empty')}` : `${t('emptyAndLogOut')}`} />
+  );
 }
 
 export default memo(CollectionsPage);
